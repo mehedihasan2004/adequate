c809b91a30c8356bc5c0a56d4d6c85daf06f1221
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-03-10 15:31:00 +0600

    Doctor find, search and filtering page created and doctor card components file created - [ frontend ]

---
+++ frontend/app/_components/DoctorCard.tsx
@@ -0,0 +1,5 @@
+const DoctorCard = () => {
+  return <div>doctor card</div>;
+};
+
+export default DoctorCard;
---
+++ frontend/app/doctors/page.tsx
@@ -0,0 +1,11 @@
+import { DoctorCard } from "@/app/_components";
+
+const DoctorsPage = () => {
+  return (
+    <div>
+      <DoctorCard />
+    </div>
+  );
+};
+
+export default DoctorsPage;
